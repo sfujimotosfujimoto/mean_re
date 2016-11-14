11c62bb996415cd9f91dcdf6b8234ceccf059d00
SFujimoto <sfujimoto@MacBook-Pro.local>
2016-11-14 20:42:19 +0900

    ~#55 add routing

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,21 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from "./app.component";
 import {FormsModule} from "@angular/forms";
 import {MessageComponent} from "./messages/message.component";
 import {MessageListComponent} from "./messages/message-list.component";
 import {MessageInputComponent} from "./messages/message-input.component";
 import {MessageService} from "./messages/message.service";
+import {MessagesComponent} from "./messages/messages.component";
+import {AuthenticationComponent} from "./auth/authentication.component";
+import {HeaderComponent} from "./header.component";
+import {routing} from "./app.routing";
+import {LogoutComponent} from "./auth/logout.component";
+import {SigninComponent} from "./auth/signin.component";
+import {SignupComponent} from "./auth/signup.component";
 
 @NgModule({
     declarations: [
       AppComponent,
       MessageComponent,
       MessageListComponent,
-      MessageInputComponent
+      MessageInputComponent,
+      MessagesComponent,
+      AuthenticationComponent,
+      HeaderComponent,
+      LogoutComponent,
+      SigninComponent,
+      SignupComponent
     ],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, routing],
     bootstrap: [AppComponent],
   providers: [MessageService]
 })
