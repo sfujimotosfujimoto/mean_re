8d687b13356272ea5d2c28490646893f8643bf30
SFujimoto <sfujimoto@MacBook-Pro.local>
2016-11-20 20:04:58 +0900

    ~#78

--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 
+var User = require('../models/user');
 var Message = require('../models/message');
 
 router.get('/', function (req, res, next) {
@@ -19,22 +21,47 @@ router.get('/', function (req, res, next) {
     });
 });
 
+router.use('/', function(req, res, next) {
+  jwt.verify(req.query.token, 'secret', function(err, decoded) {
+    if (err) {
+      return res.status(401).json({
+        title: 'Not authenticated',
+        error: err
+      })
+    }
+    next();
+  })
+});
+
 router.post('/', function (req, res, next) {
-  var message = new Message({
-    content: req.body.content
-  });
-  message.save(function (err, result) {
+  var decoded = jwt.decode(req.query.token);
+  User.findById(decoded.user._id, function(err, user) {
     if (err) {
       return res.status(500).json({
         title: 'An error occurred',
         error: err
       });
     }
-    res.status(201).json({
-      message: 'Saved message',
-      obj: result
+    var message = new Message({
+      content: req.body.content,
+      user: user
     });
-  });
+    message.save(function (err, result) {
+      if (err) {
+        return res.status(500).json({
+          title: 'An error occurred',
+          error: err
+        });
+      }
+      user.messages.push(result);
+      user.save();
+      res.status(201).json({
+        message: 'Saved message',
+        obj: result
+      });
+    });
+  })
+
 });
 
 router.patch('/:id', function (req, res, next) {
